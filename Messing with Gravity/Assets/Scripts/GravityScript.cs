61f12833f086880ef7fc4cd12533f45aedae0292
Unknown <paosalcedo@gmail.com>
2017-02-13 16:11:43 -0500

    Added initial gravity changing

---
+++ Messing with Gravity/Assets/Scripts/GravityScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityScript : MonoBehaviour {
+
+	public float posY;
+	public float posX;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//add an upward translate to the player.
+		if (Input.GetKeyDown (KeyCode.W)) {
+			//transform.Translate (Vector3.down * 50f * Time.deltaTime);
+			transform.position = new Vector2 (posX, posY += 9.8f * Time.deltaTime);
+			Debug.Log ("W was pressed");
+		}
+
+		if(Input.GetKeyDown (KeyCode.S)){
+			//transform.Translate(Vector3.up * 50f * Time.deltaTime);
+			transform.position = new Vector2 (posX += 9.8f * Time.deltaTime, posY);
+			Debug.Log ("S was pressed");
+		}
+	}
+}
