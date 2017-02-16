aa3ab8b74c16a966d3fbb47bf5b69aa50bf2a4a1
Unknown <paosalcedo@gmail.com>
2017-02-16 10:31:25 -0500

    Added functions that change the direction of gravity

--- Messing with Gravity/Assets/Scripts/GravityScript.cs
+++ Messing with Gravity/Assets/Scripts/GravityScript.cs
@@ -4,43 +4,111 @@ using UnityEngine;
 
 public class GravityScript : MonoBehaviour {
 
-	public float posY;
-	public float posX;
-	public float gravity;
-	public float velocity;
+	float posY;
+	float posX;
+	public float gravityY;
+	public float gravityX;
+	public float velocityY;
+	public float velocityX;
+
+	public KeyCode upKey = KeyCode.W;
+	public KeyCode downKey = KeyCode.S;
+	public KeyCode leftKey = KeyCode.A;
+	public KeyCode rightKey = KeyCode.D;
+
 	// Use this for initialization
 	void Start () {
-		gravity = 9.8f;
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
+//
+//		velocityY = velocityY - gravityY * Time.deltaTime; 
+//		velocityX = velocityX - gravityX * Time.deltaTime;
 
 
-	
-		velocity = velocity - gravity * Time.deltaTime; 
-		//transform.position = new Vector2 (posX, posY -= velocity * Time.deltaTime);
+/*------------------------------------------------------------------------------------
+------------------------------TRANSLATE MOVEMENT--------------------------------------
+-------------------------------------------------------------------------------------*/
+//		//TRANSLATE-based movement along the Y-axis.
+	//	transform.position = new Vector2 (posX, posY -= velocityY * Time.deltaTime);
+//
+//		//TRANSLATE-based movement along the Y-axis.
+	//	transform.position = new Vector2 (posX -= velocityX * Time.deltaTime, posY);
 
-		//add an upward translate to the player.
+//		if (Input.GetKey (KeyCode.X)) {
+//			gravityY = 0f;
+//			velocityY = 0f;
+//			Debug.Log ("X was pressed");
+//		}
+//
+//		if (Input.GetKey (KeyCode.W)) {
+//			gravityY = 9.8f;
+//			//transform.Translate (Vector3.down * 50f * Time.deltaTime);
+//			Debug.Log ("W was pressed");
+//		} 
+//
+//		if(Input.GetKey(KeyCode.S)){
+//			//transform.Translate(Vector3.up * 50f * Time.deltaTime);
+//			gravityY = -9.8f;
+//			Debug.Log ("S was pressed");
+//		} 
+//
+//		if (Input.GetKey (KeyCode.A)) {
+//			gravityX = -9.8f;
+//			//transform.Translate (Vector3.down * 50f * Time.deltaTime);
+//			Debug.Log ("A was pressed");
+//		}  
+//
+//		if(Input.GetKey (KeyCode.D)){
+//			//transform.Translate(Vector3.up * 50f * Time.deltaTime);
+//			gravityX = 9.8f;
+//			Debug.Log ("D was pressed");
+//		}  
 
-		if (Input.GetKey (KeyCode.X)) {
-			gravity = 0f;
-			velocity = 0f;
-			Debug.Log ("X was pressed");
+
+/*------------------------------------------------------------------------------------
+------------------------------GRAVITY EXPERIMENT--------------------------------------
+-------------------------------------------------------------------------------------*/			
+
+		if(Input.GetKeyDown(KeyCode.U)){
+			GravUp ();
 		}
 
-		if (Input.GetKey (KeyCode.W)) {
-			gravity = 9.8f;
-			//transform.Translate (Vector3.down * 50f * Time.deltaTime);
-			Debug.Log ("W was pressed");
+		if(Input.GetKeyDown(KeyCode.J)){
+			GravNormal ();
 		}
 
-		if(Input.GetKey (KeyCode.S)){
-			//transform.Translate(Vector3.up * 50f * Time.deltaTime);
-			gravity = -9.8f;
-			Debug.Log ("S was pressed");
+		if(Input.GetKeyDown(KeyCode.H)){
+			GravLeft ();
 		}
-			
+
+		if(Input.GetKeyDown(KeyCode.K)){
+			GravRight ();
+		}
+
+
 	}
+
+	//Sets normal gravity (down on the Y-axis)
+	void GravNormal() {
+		Physics.gravity = new Vector3(0, -9.8F, 0);
+	}
+
+	//Sets gravity to pull up on the Y-axis
+	void GravUp(){
+		Physics.gravity = new Vector3(0, 9.8F, 0);
+	}
+
+	//Sets gravity to pull left on the X-axis
+	void GravLeft(){
+		Physics.gravity = new Vector3 (-9.8f, 0, 0);
+	}
+
+	//Sets gravity to pull right on the X-axis
+	void GravRight(){
+		Physics.gravity = new Vector3 (9.8f, 0, 0);
+	}
+
 }
+
